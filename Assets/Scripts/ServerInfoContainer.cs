089a354e96845fcee124dd3244989594faaad42b
Esia Belbachir <esia.belbachir@protonmail.com>
2017-04-10 08:42:54 +0200

    Replaced character model and modified kinect and mobile menus

--- Assets/Scripts/ServerInfoContainer.cs
+++ Assets/Scripts/ServerInfoContainer.cs
@@ -8,6 +8,9 @@ public class ServerInfoContainer : MonoBehaviour {
     public InputField AddressField;
     public InputField PortField;
 
+	public string Address;
+	public string Port;
+
     public string getParsedAddress() {
          return this.AddressField.text.ToString();
     }
@@ -15,6 +18,14 @@ public class ServerInfoContainer : MonoBehaviour {
     public int getParsedPort(){
         return int.Parse(this.PortField.text.ToString());
     }
+
+    public void UpdateAddress() {
+		this.Address = this.AddressField.text.ToString();
+	}
+
+    public void UpdatePort(){
+		this.Port = this.PortField.text.ToString();
+	}
 
     // Use this for initialization
     void Start () {
